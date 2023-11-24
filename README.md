f258b74767a244ab1c818e2bbe7e82ebda7d920f
Jaime Ansorena <jaimeansorenac@gmail.com>
2023-11-24 18:43:15 -0300

    update proyecto.cpp

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ Para compilar el código, se debe realizar make en el directorio correspondiente
 
 En caso de no contar con makefile utilizar lo siguiente:
 
-	$ g++ -std=c++20 -o proyecto.out proyecto_nuevo.cpp
+	$ g++ -std=c++20 -o proyecto.out proyecto.cpp
+	
 Y ejecutar con:
 
 	$ /usr/bin/time -v ./proyecto.out [directorio] [umbral]
--- proyecto.cpp
+++ proyecto.cpp
@@ -1,31 +1,163 @@
 #include <condition_variable>
 #include <iostream>
-#include <vector>
-#include <fstream>
-#include <string>
-#include <cstring>
-#include <thread>
-#include <semaphore>
-#include <dirent.h>
 #include <queue>
+#include <fstream>
+#include <dirent.h>
 #include <mutex>
-#include <condition_variable>
+#include <thread>
+#include <utility>
+#include <semaphore>
+#include <cstring>
 
 using namespace std;
 #define epsilon 1e-6
 
-// Variables Globales
-std::counting_semaphore<1> sem(1);
-queue<string> colaGenomas;
-mutex mtx;
-condition_variable cv;
+float calcularCG(string fileName);
+void leerDirectorio(string directorio, vector<pair<string,string>> &genomas);
+
+class ColaCompartidaMutex {
+private:
+    mutex mtx;
+    condition_variable cv;
+    queue<pair<string,float>> cola;
+    bool terminado = false;
+public:
+    ColaCompartidaMutex() {}
+    ~ColaCompartidaMutex() {}
+
+    void pushGenoma(string directorio, string archivo, float umbral){
+        float CG_ratio = calcularCG(directorio+archivo);
+
+        if (CG_ratio - umbral < -epsilon) {
+            return;
+        } 
+
+        unique_lock<mutex> lock(mtx);
+        cola.push({archivo,CG_ratio});
+        cv.notify_one();
+    }
+
+    void printGenoma(){
+        unique_lock<mutex> lock(mtx);
+
+        while (!terminado) {
+            cv.wait(lock, [this] { return !cola.empty() || terminado; });
+            if (!cola.empty()) {
+                pair<string, float> genoma = cola.front();
+                cola.pop();
+                cout << genoma.first << " " << genoma.second << endl;
+            }
+        }
+    }
+
+    void terminar(){
+        unique_lock<mutex> lock(mtx);
+        terminado = true;
+        cv.notify_all();
+    }
+};
+
+class ColaCompartidaSemaforo {
+private:
+    counting_semaphore<1> sem;
+    queue<pair<string,float>> cola;
+    bool terminado = false;
+public:
+    ColaCompartidaSemaforo() : sem(1){}
+    ~ColaCompartidaSemaforo() {}
+
+    void pushGenoma(string directorio, string archivo, float umbral){
+        float CG_ratio = calcularCG(directorio+archivo);
+
+        if (CG_ratio - umbral < -epsilon) {
+            return;
+        } 
+
+        sem.acquire();
+        cola.push({archivo,CG_ratio});
+        sem.release();
+    }
+
+    void printGenoma(){
+        while(!terminado){
+            if(!cola.empty()){
+                sem.acquire();
+                pair<string, float> genoma = cola.front();
+                cola.pop();
+                cout << genoma.first << " " << genoma.second << endl;
+                sem.release();
+            }
+        }
+    }
+
+    void terminar(){
+        terminado = true;
+    }
+};
+
+int main(int argc, char const *argv[]){
+    if(argc != 3){
+		cout << "Fallo en ingreso de parametros" << endl;
+		exit(EXIT_FAILURE);
+    }
+    float umbral = atof(argv[2]);
+    if( 0 > umbral || umbral > 1){
+        cout << "Error: Umbral invalido" << endl;
+        exit(EXIT_FAILURE);
+    }
+	char* directorio;
+    vector<pair<string,string>> genomas;
+
+    leerDirectorio(string(argv[1]), genomas);
+
+    int genomaSize = genomas.size();
+    /*****************************************************************************************/
+    cout << "Ingrese opcion, 1: Mutex, 2: Semaforo: ";
+    int choice; cin >> choice;
+    if (choice == 1){
+        cout << "Cola Compartida Mutex" << endl;
+        ColaCompartidaMutex ccm;
+        thread printThreadMut(&ColaCompartidaMutex::printGenoma, &ccm);
+        vector<thread> threads_ccm;
+        for (int i = 0; i < genomaSize; i++) {
+            threads_ccm.emplace_back(&ColaCompartidaMutex::pushGenoma, &ccm, genomas[i].first,genomas[i].second, umbral);
+        }
+
+        for (auto& t : threads_ccm) {
+            t.join();
+        }
+        
+        ccm.terminar();
+        printThreadMut.join();
+    }else if (choice == 2){
+        cout << "Cola Compartida Semaforo" << endl;
+        ColaCompartidaSemaforo ccs;
+        thread printThreadSem(&ColaCompartidaSemaforo::printGenoma, &ccs);
+        vector<thread> threads_ccs;
+        for (int i = 0; i < genomaSize; i++) {
+            threads_ccs.emplace_back(&ColaCompartidaSemaforo::pushGenoma, &ccs, genomas[i].first,genomas[i].second, umbral);
+        }
+
+        for (auto& t : threads_ccs) {
+            t.join();
+        }
+
+        ccs.terminar();
+        printThreadSem.join();
+    }else{
+        cout << "Error: opcion invalida" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
 
 float calcularCG(string fileName){
     ifstream inputFile(fileName);
-	
+    
     if (!inputFile.is_open()) {
         cerr << "Error al abrir el archivo: " << fileName << endl;
-		cerr << strerror(errno) << endl;
+        cerr << strerror(errno) << endl;
         return 1;
     }
 
@@ -52,78 +184,20 @@ float calcularCG(string fileName){
     return float(countC+countG)/float(countCGAT);
 }
 
-void leerDirectorio(string directorio, vector<string> &genomas) {
-	DIR *dir;
-	struct dirent *diread;
-	if ((dir = opendir(directorio.c_str())) != NULL) {
-        while ((diread = readdir(dir)) != NULL) {
+void leerDirectorio(string directorio, vector<pair<string,string>> &genomas) {
+    DIR *dir;
+    struct dirent *diread;
+    if ((dir = opendir(directorio.c_str())) != NULL) {
+        while ((diread = readdir(dir)) != NULL){
             // Filtra las entradas "." y ".."
             if (diread->d_name != string(".") && diread->d_name != string("..") && diread->d_type == DT_REG) {
                 string s = diread->d_name;
-                genomas.push_back(string(directorio) + "/" + s);
-            } else if (diread->d_type == DT_DIR && diread->d_name != string(".") && diread->d_name != string("..")) {
-				cout << "directorios: " << diread->d_name << endl;
-				string subDirPath = directorio + "/" + diread->d_name;
-                leerDirectorio(subDirPath, genomas);
-			}
+                genomas.push_back({directorio + "/",s});
+            }
         }
         closedir(dir);
     } else {
         perror("opendir");
-        return;
+        exit(1);
     }
 }
-
-void leer_genoma_semaforo(string archivo, float umbral) {
-    float CG_ratio = calcularCG(archivo);
-
-    if (CG_ratio - umbral < -epsilon) {
-        cout << archivo << ": no entra" << endl;
-        return;
-    } 
-
-    sem.acquire();
-    colaGenomas.push(archivo);
-    cout << archivo << " % CG: " << CG_ratio << endl;
-    sem.release();
-    
-}
-
-void leer_genoma_mutex_cv(string archivo, float umbral){
-    float CG_ratio = calcularCG(archivo);
-
-    if (CG_ratio - umbral < -epsilon) {
-        cout << archivo << ": no entra" << endl;
-        return;
-    } 
-
-    unique_lock<mutex> lck(mtx);
-    colaGenomas.push(archivo);
-    cout << archivo << " % CG: " << CG_ratio << endl;
-    cv.notify_one();
-}
-
-int main(int argc, char const *argv[]){
-    if(argc != 3){
-		cout << "Fallo en ingreso de parametros" << endl;
-		exit(EXIT_FAILURE);
-	}
-    float umbral = atof(argv[2]);
-	char* directorio;
-    vector<string> genomas;
-	
-    leerDirectorio(string(argv[1]),genomas);
-	
-    int genomaSize = genomas.size();
-    vector <thread> threads;
-    for(int i = 0; i < genomaSize; i++){
-        //cout << genomas[i] << endl;
-        threads.emplace_back(leer_genoma_semaforo, genomas[i], umbral);
-        //threads.emplace_back(leer_genoma_mutex_cv, genomas[i], umbral);
-    }
-    for(auto &t : threads){
-		t.join();
-	}
-    return 0;
-}
-
